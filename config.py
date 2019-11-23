2bb9eff450eecd947523fadfca50f9d0e536a03b
njoroge33 <johngichuhi769@gmail.com>
2019-11-23 23:52:02 +0300

    initialize the database

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 class Config:
     
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:limuru33@localhost/pitch'
 
 class ProdConfig(Config):
     pass
