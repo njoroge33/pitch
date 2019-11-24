b8dffed0a2692ed108d85fb3e1e596dba4aa883e
njoroge33 <johngichuhi769@gmail.com>
2019-11-24 19:40:33 +0300

    create a login route

--- app/main/views.py
+++ app/main/views.py
@@ -1,14 +1,17 @@
 from . import main
-from flask import render_template
+from flask import render_template, redirect, url_for
 from .forms import *
 from ..models import *
 
 @main.route('/')
 def index():
+
     return render_template('index.html')
+
 
 @main.route('/signup' , methods=['GET', 'POST'])
 def signup():
+
     reg_form = RegistrationForm()
 
     #updates the db if the validation was successful
@@ -19,6 +22,17 @@ def signup():
         user = User(username=username, password=password)
         db.session.add(user)
         db.session.commit()
-        return 'inserted into db!'
+        return redirect(url_for('login'))
         
     return render_template('signup.html', form=reg_form)
+
+
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    login_form = LoginForm()
+
+    #login if validation is successful
+    if login_form.validate_on_submit():
+        return "logged in!"
+
+    return render_template('login.html', form=login_form)
