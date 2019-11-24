035cf1580bb1f7ebd6f516322676c74ca2ad9f08
njoroge33 <johngichuhi769@gmail.com>
2019-11-24 17:43:04 +0300

    create a connection between my db and views file

--- app/main/views.py
+++ app/main/views.py
@@ -10,19 +10,19 @@ def index():
 @main.route('/signup' , methods=['GET', 'POST'])
 def signup():
     reg_form = RegistrationForm()
-    # if reg_form.validate_on_submit():
-    #     username = reg_form.username.data
-    #     password = reg_form.password.data
+    if reg_form.validate_on_submit():
+        username = reg_form.username.data
+        password = reg_form.password.data
 
-    #     #Check if username exists
-    #     user_object = User.query.filter_by(username=username).first()
-    #     if user_object:
-    #         return "username already taken!"
+        #Check if username exists
+        user_object = User.query.filter_by(username=username).first()
+        if user_object:
+            return "username already taken!"
         
-    #     user = User(username=username, password=password)
-    #     db.session.add(user)
-    #     db.session.commit()
-    #     return 'inserted into db!'
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return 'inserted into db!'
 
 
     return render_template('signup.html', form=reg_form)
