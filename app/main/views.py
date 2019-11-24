d21be6510496ca7f8636d59c3f314465f2168369
njoroge33 <johngichuhi769@gmail.com>
2019-11-24 13:15:15 +0300

    create a class for the signup form

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,12 @@
 from . import main
 from flask import render_template
+from .forms import *
 
-@main.route('/', methods=['GET', 'POST'])
+@main.route('/')
 def index():
-    
     return render_template('index.html')
+
+@main.route('/signup' , methods=['GET', 'POST'])
+def signup():
+    reg_form = RegistrationForm()
+    return render_template('signup.html', form=reg_form)
