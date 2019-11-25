0fb94dc848e78b4635b5c3919d77097e9264ae3e
njoroge33 <johngichuhi769@gmail.com>
2019-11-25 10:07:21 +0300

    add flashing

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from . import main
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from werkzeug.security import generate_password_hash
 from flask_login import login_user, current_user, login_required, logout_user
 from .forms import *
@@ -23,6 +23,9 @@ def signup():
         user = User(username=username, password=password)
         db.session.add(user)
         db.session.commit()
+
+        flash('Registered successfully.Please login', 'success')
+
         return redirect(url_for('main.login'))
         
     return render_template('signup.html', form=reg_form)
@@ -48,14 +51,14 @@ def profile(uname):
     user = User.query.filter_by(username = uname).first()
 
     if not current_user.is_authenticated:
-            return "please login flask!"
-        # return "wellcome!"
+        flash('please login', 'danger')
+        return redirect(url_for('main.login'))
 
     return render_template("profile.html", user = user)
 
 @main.route('/logout', methods=['GET'])
 def logout():
     logout_user()
+    flash('you have logged out successfuly', 'success')
 
-    return "logged out"
-
+    return redirect(url_for('main.login'))
